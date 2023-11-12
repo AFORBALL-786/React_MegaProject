d3d34d13ae42c7fc472679c80770d3b5ca586c8d
John Doe <aforball76@gmail.com>
2023-11-12 13:09:57 +0545

    reducer and some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then( (userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } 
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
 
   return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
+      !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            <Outlet/>
+          </main>
+          <Footer/>
+        </div>
+
+      </div>) : null
   )
 }
 
